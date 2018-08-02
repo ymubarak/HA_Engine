0b0c7e4ecae703408aff6b529f2f1d36726e445d
Youssef A.Mubarak <engyousefahmed777@gmail.com>
2018-08-03 01:03:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 "use strict";
-
+// requirements
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require('https');
-
+// API keys
 const API_KEY = 'AIzaSyAHAPL8XQGg7SWSyvCzfYSFrpv1xMHJpNA';
+// Error Messages
 const Unkown_Input_ERR = "Seems like some problem. Speak again.";
 const Invalid_ERR = "Unkown input";
+
+// define functions
+function get_address(lat, lng) {
+      var address = '';
+      var web_site = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key=' 
+      + API_KEY;
+      https.get(web_site, (resp) => {
+      let data = '';
+   
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+   
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        console.log("Proper address:");
+        address = JSON.parse(data).results[0].formatted_address;
+        console.log(msg);
+      });
+
+      }).on("error", (err) => {
+        address = Unkown_Input_ERR
+      });
+
+      return address;
+}
+
+function get_lat_lng(){
+      var web_site = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + API_KEY;     
+      https.post(web_site, (resp) => {
+      let data = '';
+   
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      
+      var out = [];
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        out[0] = JSON.parse(data).location.lat;
+        out[1] = JSON.parse(data).location.lng;
+      });
+
+      }).on("error", (err) => {
+        return Unkown_Input_ERR;
+      });
+      
+      return out;
+}
+
 
 const restService = express();
 restService.use(
@@ -29,25 +82,8 @@ restService.post("/location", function(req, res) {
     msg = Invalid_ERR;
   else {
     // connect to google api
-    var web_site = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=' + API_KEY;
-    https.get(web_site, (resp) => {
-      let data = '';
-   
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-   
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log("Proper msg:");
-        msg = JSON.parse(data).results[0].formatted_address;
-        console.log(msg);
-      });
-
-    }).on("error", (err) => {
-        msg = Unkown_Input_ERR
-    });
+    var latlng = get_lat_lng();
+    get_address(latlng[0],latlng[1]);
   }
 // return json response
 return res.json({
@@ -85,25 +121,8 @@ restService.get("/location", function(req, res) {
     msg = Invalid_ERR;
   else {
     // connect to google api
-    var web_site = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=' + API_KEY;
-    https.get(web_site, (resp) => {
-      let data = '';
-   
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-   
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        console.log("Proper msg:");
-        msg = JSON.parse(data).results[0].formatted_address;
-        console.log(msg);
-      });
-
-    }).on("error", (err) => {
-        msg = Unkown_Input_ERR
-    });
+    var latlng = get_lat_lng();
+    get_address(latlng[0],latlng[1]);
   }
 // return json response
 return res.json({
